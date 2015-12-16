d5fd6d193e1d273f26d1594229ff72dca1910cd0
ivanhristov93 <tik9@abv.bg>
2015-12-16 21:23:47 +0200

    asd

---
+++ azremontiram/mni.js
@@ -0,0 +1,9 @@
+var output = '';
+var input = ['soft', 'intellect', 'academy', 2015];
+
+for(var i = 0; i<input.length; i + 1){
+	console.log('dobavqm', input[i]);
+	output = output + ' ' + input[i];
+};
+
+console.log('output')
