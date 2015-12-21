4921c0165e4d0a9dc34b0688a89a413ff138708d
ivanhristov93 <tik9@abv.bg>
2015-12-21 16:32:37 +0200

    remont

--- azremontiram/mni.js
+++
@@ -1,9 +0,0 @@
-var output = '';
-var input = ['soft', 'intellect', 'academy', 2015];
-
-for(var i = 0; i<input.length; i + 1){
-	console.log('dobavqm', input[i]);
-	output = output + ' ' + input[i];
-};
-
-console.log('output')
